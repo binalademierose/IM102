06991db8a67641e15de202e9cabff22875fa4285
binalademierose <binalademierosejean@gmail.com>
2025-09-01 20:26:57 +0800

    Update BinalaDemie_Assignment1.py

--- BinalaDemie_Assignment1.py
+++ BinalaDemie_Assignment1.py
@@ -3,10 +3,9 @@ while True:
     columns = int(input("Enter number of columns: "))
     search = int(input("Enter the number you want to search: "))
 
-    if rows or columns < 1:
+    if rows < 1 or columns < 1:
         break
     else:
-        
          for x in range (1, rows + 1):
             for y in range(1, columns + 1):
                 value = x * y
