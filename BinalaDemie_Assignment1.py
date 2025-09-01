1fe6613fd214b9917d8bcd0f46d0d82569986201
Umal Mktg Urdaneta <binalademierosejean@gmail.com>
2025-09-01 19:58:24 +0800

    v1

---
+++ BinalaDemie_Assignment1.py
@@ -0,0 +1,20 @@
+while True: 
+    rows = int(input("Enter number of rows: "))
+    columns = int(input("Enter number of columns: "))
+    search = int(input("Enter the number you want to search: "))
+
+    if rows and columns < 1:
+        break
+    else:
+        
+         for x in range (1, rows + 1):
+            for y in range(1, columns + 1):
+                value = x * y
+                if value == search:
+                    print(f"[{value}]", end="\t")
+                else:
+                    print(value, end="\t")
+            print()
+
+
+    
